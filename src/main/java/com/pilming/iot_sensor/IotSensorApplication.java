6ed47803bd44e1a8ce9b0269623cc5235e9ad818
pilming44 <pilming44@naver.com>
2025-03-02 02:42:37 +0900

    feat: 프로젝트 초기세팅

---
+++ src/main/java/com/pilming/iot_sensor/IotSensorApplication.java
@@ -0,0 +1,13 @@
+package com.pilming.iot_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pilming/iot_sensor/IotSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilming.iot_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
