3b2a22aaa59effae2aa25489741dfaf0cde4ed36
pilming44 <pilming44@naver.com>
2025-05-21 23:08:45 +0900

    chore: SensorChartResponseDto에 @Jacksonized추가

--- src/main/java/com/pilming/iot_sensor/dto/SensorChartResponseDto.java
+++ src/main/java/com/pilming/iot_sensor/dto/SensorChartResponseDto.java
@@ -3,17 +3,20 @@ package com.pilming.iot_sensor.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Getter
 @Builder
+@Jacksonized
 @AllArgsConstructor
 public class SensorChartResponseDto {
     private List<DatasetDto> datasets;
 
     @Getter
     @Builder
+    @Jacksonized
     public static class DatasetDto {
         private String label;
         private List<String> timestamps;
